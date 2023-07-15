d26868f391783d3b82feab42b460fa11301d8605
tashirohiroki45483 <kirigiri700@gmail.com>
2023-07-15 15:35:15 +0000

    sukkiri0

---
+++ hello2.java
@@ -0,0 +1,16 @@
+public class Main {
+	public static void main(String[] args) { 
+		System.out.println("すがわら");
+		System.out.println("31歳です");
+		System.out.println("お酒が好きです");
+		System.out.println("31 + 31の計算をします");
+		System.out.println(31 + 31);
+		System.out.println(35 - 10);
+		System.out.println(-5 * 2);
+		System.out.println(6 * 6 * 3.14);
+		System.out.println("答えは" + 64);
+		int x;
+		x = 6;
+		System.out.println(x * x * 3.14);
+	}
+}
