66904bab257f7ddcea4ad7df9e9b2ff5ed3f5e8f
tashirohiroki45483 <kirigiri700@gmail.com>
2023-07-31 14:32:40 +0000

    renshu1-2

---
+++ renshu1-2.java
@@ -0,0 +1,11 @@
+public class Main{
+  public static void main (String[] args){
+    int a;
+    a = 3;
+    int b;
+    b = 5;
+    int c;
+    c = a * b;
+    System.out.println("横幅" + a + "縦幅" + b + "の長方形の面積は、" + c );
+  }
+}
