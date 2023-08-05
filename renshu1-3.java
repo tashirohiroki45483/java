9e343d20e942cdf1aa0cd44adfcef19ad6d5234d
tashirohiroki45483 <kirigiri700@gmail.com>
2023-08-05 03:46:13 +0000

    renshu1-3

---
+++ renshu1-3.java
@@ -0,0 +1,14 @@
+public class Main {
+  public static void main (String [] args){
+    boolean result = true;
+    char name = '辰';
+    double pie = 3.14;
+    long number = 314159265853979L;
+    String msg = "ミナトの攻撃！的に15ポイントのダメージを与えた。";
+    System.out.println(result);
+    System.out.println(name);
+    System.out.println(pie);
+    System.out.println(number);
+    System.out.println(msg);
+  }
+}
