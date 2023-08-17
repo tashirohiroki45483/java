4d5cec73cd53cb763f6ee99933248bbfca59e51b
tashirohiroki45483 <kirigiri700@gmail.com>
2023-08-17 09:20:36 +0000

    11章終わり

---
+++ renshu11-1/Book.java
@@ -0,0 +1,12 @@
+public class Book extends TangibleAsse{
+  String isbn;
+  //コンストラクタ
+  public Book(String name, int price, String color, String isbn){
+    super(name, price, color);
+    this.isbn = isbn;
+  }
+  //メソッド
+  public String getIsbn(){
+    return this.isbn;
+  }
+}
---
+++ renshu11-1/TangibleAsset.java
@@ -0,0 +1,19 @@
+public abstract class TangibleAsset extends Asset implements Thing{
+  String color;
+  double weight;
+  //コンストラクタ
+  public TangibleAsset(String name, int price, String color){
+    super(name, price);
+    this.color = color;
+  }
+  //メソッド
+  public String getColor(){
+    return this.color;
+  }
+  public double getWeight(){
+    return this.weight;
+  }
+  public void setWeight(double weight){
+    this.weight = weight
+  }
+}
